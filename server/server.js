4e307b0743270c6726ff0ed67a16bd0e257b8ac6
Sutthirath Sean Vilaysane <seanvilaysane@gmail.com>
2019-07-30 13:16:22 -0700

    apollo/express server set and now working on mongoose instance

---
+++ server/server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const { ApolloServer } = require("apollo-server-express");
+
+const mongoose = require("./config/database");
+const typeDefs = require("./modules/post/graphqlSchema");
+const resolvers = require("./modules/post/resolvers");
+
+const server = new ApolloServer({ typeDefs, resolvers });
+
+const app = express();
+const PORT = 3000; // Secure port before deploy
+
+server.applyMiddleware({ app });
+
+app.listen(PORT, () => {
+  console.log(
+    `Server running on http://localhost:${port}${server.graphqlPath}`
+  );
+});
