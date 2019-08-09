a2ceb9065898c26ee509635d3d1bc192223c6016
Sutthirath Sean Vilaysane <seanvilaysane@gmail.com>
2019-08-09 12:15:59 -0700

    graphql schema

---
+++ server/modules/post/graphqlSchema.js
@@ -0,0 +1,16 @@
+const { gql } = require("apollo-server-express");
+
+const typeDefs = gql`
+  type Post {
+    _id: ID
+    title: String
+    content: String
+  }
+  type Query {
+    posts: [Post]
+  }
+  type Mutation {
+    addPost(title: String!, content: String!): Post
+  }
+`;
+module.exports = typeDefs;
---
+++ server/modules/post/resolvers.js
@@ -0,0 +1,18 @@
+const Post = reqiure("./models/post");
+
+const resolvers = {
+  Query: {
+    posts: () => Post.find({})
+  },
+  Mutation: {
+    addPost: (parent, post) => {
+      const newPost = new Post({
+        title: post.title,
+        content: post.content
+      });
+      return newPost.save();
+    }
+  }
+};
+
+module.exports = resolvers;
